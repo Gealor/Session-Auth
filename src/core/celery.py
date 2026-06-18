9f3bf3bea3fd3a80d5a07e697a42992a206a5d0c
Gealor <gzazulya@gmail.com>
2026-06-18 22:23:17 +0400

    return previous crontab

--- src/core/celery.py
+++ src/core/celery.py
@@ -19,7 +19,7 @@ def prepare_celery() -> Celery:
     celery.conf.beat_schedule = {
         "cleanup_expired_tokens": {
             "task": "src.tasks.clear_expired_tokens.cleanup_expired_tokens",
-            "schedule": crontab(minute="20", hour="18", day_of_week="1,3,4,5,6") # тут мы выполняем задачу в 11 часов 30 минут по UTC, в понедельник, среду, пятницу и субботу
+            "schedule": crontab(minute="30", hour="11", day_of_week="1,3,5,6"), # тут мы выполняем задачу в 11 часов 30 минут по UTC, в понедельник, среду, пятницу и субботу
         }
     }
 
