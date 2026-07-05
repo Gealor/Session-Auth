0836b5188913397bc70387524121ae3c34b6d127
Gealor <gzazulya@gmail.com>
2026-07-05 23:10:04 +0400

    comment about __init__.py in tasks

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -1,9 +1,13 @@
-__all__ = (
+__all__ = ( # Этот __init__.py нужен, чтобы taskiq воркер увидел эти задачи (мы указали явно --fs-discover --tasks-pattern "src/tasks")
     "cleanup_expired_tokens",
     "send_email_for_verification",
     "log_action",
 )
 
+import sys
+
 from .clear_expired_tokens import cleanup_expired_tokens
 from .email_send_tasks import send_email_for_verification
 from .log_tasks import log_action
+
+print(sys.argv)
